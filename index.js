973d308049591f32cee9934ef39d52d2281eb918
Darcy Linde <lindedarcy@gmail.com>
2019-05-01 13:45:59 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(array){
+  let record = array.find(i => i.result === "W");
+  if (record != undefined){
+    return record.year;
+  } else {
+    return undefined;
+  }
+}
